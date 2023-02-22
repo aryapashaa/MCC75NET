a3d817cd675fdf4b57762ed9478b61859a60d3bd
Arya Pasha Gunawan <aryapashaa49@gmail.com>
2023-02-22 11:54:54 +0700

    menambahkan UniversityRepository

---
+++ Repositories/EducationRepository.cs
@@ -0,0 +1,32 @@
+﻿using MCC75NET.Models;
+using MCC75NET.Repositories.Interface;
+
+namespace MCC75NET.Repositories;
+
+public class EducationRepository : IRepository<int, Education>
+{
+    public int Delete(int key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Education> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Education GetById(int key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Insert(Education entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Update(Education entity)
+    {
+        throw new NotImplementedException();
+    }
+}
